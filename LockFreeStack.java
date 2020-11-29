e420df1a99077b464db784944eebaf6e72f27076
Camilo Lozano <camilozano98@gmail.com>
2020-11-29 16:29:36 -0500

    Problem 1 solved

---
+++ LockFreeStack.java
@@ -0,0 +1,77 @@
+import java.util.EmptyStackException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+
+public class LockFreeStack<T> {
+    AtomicReference<Node<T>> top = new AtomicReference<Node<T>>(null);
+    AtomicInteger atomicNumOps = new AtomicInteger(0);
+    int numOps = 0;
+
+    final boolean useAtomicOpsCounter = false;
+
+    public void push(T value){
+        Node<T> node = new Node<T>(value);
+        
+        while (true){
+            Node<T> oldTop = this.top.get();
+            node.next = oldTop;
+            if (this.top.compareAndSet(oldTop, node)){
+                this.incrementCounter();
+                break;
+            }
+        }
+    }
+
+    public T pop() throws EmptyStackException {
+
+        while (true){
+
+            Node<T> returnNode = null;
+
+            Node<T> oldTop = this.top.get();
+            if (oldTop == null){
+                throw new EmptyStackException();
+            }
+            Node<T> newTop = oldTop.next;
+            if (this.top.compareAndSet(oldTop, newTop)){
+                returnNode = oldTop;
+            }
+
+            if (returnNode != null){
+                this.incrementCounter();
+                return returnNode.value;
+            }
+
+        }
+    }
+
+    private void incrementCounter(){
+        if (this.useAtomicOpsCounter){
+            this.atomicNumOps.incrementAndGet();
+        }
+        else{
+            this.numOps++;
+        }
+    }
+
+    public int getNumOps(){
+        if (this.useAtomicOpsCounter){
+            return this.atomicNumOps.get();
+        }
+        return this.numOps;
+    }
+
+
+}
+
+class Node<T> {
+    public T value;
+    public Node<T> next;
+    public Node(T value){
+        this.value = value;
+        this.next = null;
+    }
+    
+}
+
---
+++ ProblemOne.java
@@ -0,0 +1,80 @@
+import java.util.EmptyStackException;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProblemOne {
+    static LockFreeStack<Integer> stack;
+    static final int OPS_PER_THREAD = 150000;
+    static final int NUM_THREADS = 8;
+    static final int INIT_SIZE = 50000;
+
+    static Thread[] threads = new Thread[NUM_THREADS];
+    public static void main(String[] args) throws InterruptedException {
+        stack = new LockFreeStack<Integer>();
+        long start, end;
+
+        for (int i=0; i<INIT_SIZE; i++){
+            int randInt = ThreadLocalRandom.current().nextInt();
+            stack.push(randInt);
+        }
+
+        for (int i=0; i<NUM_THREADS; i++){
+            threads[i] = new RandomOps(i);
+        }
+
+        start = System.currentTimeMillis();
+        for (int i=0; i<NUM_THREADS; i++){
+            threads[i].start();
+        }
+
+        for(int i=0; i<NUM_THREADS; i++){
+            threads[i].join();
+        }
+        end = System.currentTimeMillis();
+
+        long elapsedTime = end-start;
+
+        // System.out.println(stack.getNumOps());
+        System.out.println(elapsedTime);
+
+    }
+
+    static class RandomOps extends Thread{
+
+        int thread_id;
+        int numOps;
+
+        RandomOps(int i){
+            this.thread_id = i;
+            this.numOps = 0;
+        }
+
+        public void run(){
+            while (this.numOps < OPS_PER_THREAD){
+                int randInt = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+                int op = randInt % 2;
+
+                switch (op) {
+                    case 0:
+                        stack.push(randInt);
+                        this.numOps++;
+                        break;
+                    case 1:
+                        try {
+                            stack.pop();
+                            this.numOps++;
+                        } catch (EmptyStackException e) {
+                            System.out.println("Stack Empty");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            // System.out.printf("ID: %d %d done\n", this.thread_id, this.numOps);
+
+        }
+
+    }
+
+
+}
